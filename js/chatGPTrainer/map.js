1b9a4d90414aecc49703d8969f539daeac885c7f
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2023-06-23 14:10:25 -0600

    js challenges.

---
+++ js/chatGPTrainer/map.js
@@ -0,0 +1,28 @@
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map(num => num * 2);
+console.log(doubledNumbers);
+// output>
+
+console.log(numbers);
+
+const a = {};
+const b = { key: "b" };
+const c = { key: "c" };
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(toString(b));
+
+console.log(a[b]);
+
+const zero = new Number(0);
+
+if (zero) {
+    console.log('new Number(0); is true    ');
+} else {
+    console.log('new Number(0); is false    ');
+}
+
+setInterval(() => console.log("Hi"), 1000);
+
